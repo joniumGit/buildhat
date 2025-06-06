19027849264d8839f7548713219a5982925dd885
Mark Owen <mo@list>
2025-06-06 16:05:53 +0100

    Removed unused script

--- mkrelease.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-rm -rf release
-mkdir -p release release/buildhat/ release/buildhat/docs/ release/buildhat/bhbl-pico release/buildhat/firmware-pico
-for i in `cat releasefilelist` ; do cp $i release/buildhat/$i ; done
